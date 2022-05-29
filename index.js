6bd91baafd0cd04a5fa9225b18ed6d5dc2f1765e
carolina-ross <97426898+carolina-ross@users.noreply.github.com>
2022-05-29 19:14:35 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code here
+function greet() {
+var response = prompt ("What is your name?");
+alert ("Hello, " + response);
+var age = prompt ("How old are you?");
+document.writeln(Number.parseInt(age)+"<br>");
+}
