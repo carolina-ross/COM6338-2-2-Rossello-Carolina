21dbef0a085c582f9bc34c35ce758d4ca38649e0
carolina-ross <97426898+carolina-ross@users.noreply.github.com>
2022-05-30 00:19:26 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,5 +3,9 @@ function greet() {
 var response = prompt ("What is your name?");
 alert ("Hello, " + response);
 var age = prompt ("How old are you?");
-document.writeln(Number.parseInt(age)+"<br>");
+age = parseInt(age,10);
+var result = confirm ("Have you celebrated a birthday yet this year?")
+
+var currentDate = new Date();
+var currentYear = currentDate.getFullYear();
 }
