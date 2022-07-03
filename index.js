5143fdcdca5ceb8a2eb9af992c6a079a2716dbef
carolina-ross <97426898+carolina-ross@users.noreply.github.com>
2022-07-03 15:12:51 -0400

    Update index.js

--- index.js
+++ index.js
@@ -7,13 +7,12 @@ age = parseInt(age,10);
 var result = confirm ("Have you celebrated a birthday yet this year?")
 
 // To calculate born year
-var date = new Date(); 
-var currentYear = date.getFullYear();
+var currentYear = new Date().getFullYear();
 
-if( result = true ) {
+if( result == true ) {
 year_born = currentYear - age;
 alert("You are born in " + year_born)
-} else if( result = false ) {
+} else if( result == false ) {
 calculatedYear = currentYear - age - 1;
 alert("You are born in " + calculatedYear)
 }
